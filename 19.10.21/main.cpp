1018697816a2fb50b5fcafa71875d62a613de7d3
NikitaV03 <vnr03@mail.ru>
2021-10-24 16:44:40 +0300

    add first tasks

---
+++ 19.10.21/main.cpp
@@ -0,0 +1,164 @@
+﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+void collats();
+void krivaya();
+void randomkrug();
+void nerandomkrug();
+void sumandpros();
+double rowpi();
+void rowtaylor();
+void rowsinusov();
+void goosesnrabbits();
+
+int main()
+{
+	int vihod;
+	goosesnrabbits();
+
+	std::cin >> vihod;
+	return 0;
+}
+
+void collats() {
+	int chislik;
+	std::cout << "Input number: ";
+	std::cin >> chislik;
+	while (chislik != 1) {
+		if (chislik % 2 == 1) {
+			chislik = chislik * 3 + 1;
+		}
+		else {
+			chislik /= 2;
+		}
+		std::cout << chislik << " ";
+	}
+}
+void krivaya() {
+	//doesn't work
+	//double A[2];
+	//double b[2];
+	//double c[2];
+
+	//std::cin >> a[0] >> a[1];
+	//std::cin >> b[0] >> b[1];
+	//std::cin >> c[0] >> c[1];
+	//std::cout << c[0] << " " << c[1] << std::endl;
+	//double acsq = pow((c[0] - a[0]), 2) + pow((c[1] - a[1]), 2);
+	//double bcsq = pow((c[0] - b[0]), 2) + pow((c[1] - b[1]), 2);
+	//double summa = sqrt(ac) + sqrt(bc);
+	//double right = 4 * acsq*bcsq;
+	///*double left = idk*/
+	//std::cout << summa << std::endl;
+	//std::cin >> x;
+}
+void randomkrug() {
+	double radius, a1, a2;
+	int tochnost = 3000;
+	int counterkrug = 0;
+	std::cout << "Input radius";
+	std::cin >> radius;
+	for (int i = 1; i <= tochnost; i++) {
+		a1 = (rand() / double(RAND_MAX)) * radius;
+		a2 = (rand() / double(RAND_MAX)) * radius;
+		if (a1 * a1 + a2 * a2 <= radius * radius)
+			counterkrug += 1;
+	}
+	std::cout << counterkrug << " " << tochnost << std::endl;
+	std::cout << 4 * double(counterkrug) / double(tochnost);
+
+}
+void nerandomkrug() {
+	double radius;
+	int r;
+	int counter = 0;
+	std::cin >> radius;
+	r = floor(radius);
+	for (int i = 0; i <= r; i++) {
+		for (int j = 1; j <= r; j++) {
+			if ((pow(i, 2) + pow(j, 2)) <= pow(r, 2)) {
+				counter += 1;
+			}
+		}
+	}
+	std::cout << (counter * 4 + 1) / (r * 2 - 1) * (r * 2 - 1);
+}
+void sumandpros() {
+	int chislo, sumch = 0, proisch = 1;
+	std::cin >> chislo;
+	do {
+		sumch += chislo % 10;
+		proisch *= chislo % 10;
+		chislo /= 10;
+	} while (chislo != 0);
+	std::cout << proisch << " " << sumch << std::endl;
+}
+double rowpi() {
+	double pi = 0;
+	int tochn = 10000;
+	std::cout << std::endl;
+	for (int i = 1; i <= tochn; i++) {
+		pi += (pow(-1, i + 1) * 1 / (2 * i - 1));
+	}
+	pi *= 4;
+	return pi;
+}
+void rowtaylor() {
+	double x, s = 0;
+	int n;
+	double fact = 1;
+	double pi;
+	pi = rowpi();
+	std::cin >> x >> n;
+	for (int i = 1; i <= 2 * n; i += 2) {
+		s += pow((-1), i % 4 / 2 + 1) * pow(pi, i) / fact;
+		fact *= (i + 1) * (i + 2);
+		std::cout << s << " " << i << " ";
+	}
+	std::cout << s;
+}
+void rowsinusov() {
+	double pi;
+	double lastvar, toch, x, s;
+	pi = rowpi();
+	s = 0;
+	std::cin >> x >> toch;
+	lastvar = pi;
+	for (int i = 1; i <= toch; i++) {
+		lastvar = sin(lastvar);
+		s += lastvar;
+	}
+	std::cout << s << std::endl;
+}
+void goosesnrabbits() {
+	int paws,gooses,rabbits,counter=0;
+	std::cout << "Input number of paws: ";
+	std::cin >> paws;
+	if (paws % 2 == 0) {
+		gooses = paws / 2;
+		rabbits = 0;
+		do {
+			std::cout << "gooses: " << gooses << " " << "rabbits: " << rabbits << std::endl;
+			rabbits += 1;
+			gooses -= 2;
+			counter += 1;
+		} while (gooses != -1 and gooses != -2);
+	}
+	else {
+		std::cout << "Invalid number";
+	}
+	std::cout << counter;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ 25.10.21/main.cpp
@@ -0,0 +1,132 @@
+﻿// 25.10.21.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+void sixteen();
+void time();
+
+int main()
+{
+	unsigned char exit;
+	//unsigned char n1=1;
+	//unsigned short n2=1;
+	//unsigned int n3=1;
+	//unsigned long long int n4=1;
+	//double perem=0,sum=0,sumka=0;
+	//std::cout << sizeof(n1) << sizeof(n2) << sizeof(n3) << sizeof(n4) <<  std::endl;
+	//while (sum < 4){
+	//	sumka = sum;
+	//	perem = 1 / double(n1 * n1);
+	//	sum += perem;
+	//	std::cout << sum << " " << " perem: " << perem << std::endl;
+	//	/*if (sumka = sum) {
+	//		std::cout << "S_n+1 == s_n!!!   " << n4;
+	//		break;
+	//	}*/
+	//	n1 += 1;
+	//}
+	//std::cout << n4-1 << " sum: " << sumka;
+
+	time();
+
+	std::cin >> exit;
+	return 0;
+}
+
+void sixteen() {
+	int n, ost, i = 1;
+	const char* M[] = { "0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F" };
+	std::string str = "";
+	std::cout << " Input number: ";
+	std::cin >> n;
+	while (n != 0) {
+		i += 1;
+		ost = n % 16;
+		n /= 16;
+		str = str.append(M[ost]);
+	}
+	reverse(str.begin(), str.end());
+	std::cout << str;
+}
+
+
+void time() {
+	std::string ans;
+	std::cout << "Doyou need hoursminutesseconds or days(input 'hms' or 'days')";
+	std::cin >> ans;
+	if (ans == "hms") {
+		int T1[3], T2[3]; // t[0] = h ours , t[1] = m inutes , t[2] = s econds
+		std::cin >> T1[0] >> T1[1] >> T1[2];
+		std::cin >> T2[0] >> T2[1] >> T2[2];
+		if ((T1[0] * 3600 + T1[1] * 60 + T1) - (T2[0] * 3600 + T2[1] * 60 + T2[2]) >= 0) {
+			std::cout << "equal or greater";
+		}
+		else {
+			std::cout << "lesser";
+		}
+	}
+	else {
+		int D1[3], D2[3], sum, fact, differencefirst = 0, differenceall = 0, differencelast = 0, numofdays[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+		std::cin >> D1[0] >> D1[1] >> D1[2]; // d[0] = day , d[1] = month, d[2] = year
+		std::cin >> D2[0] >> D2[1] >> D2[2];
+		if ((D1[2] < D2[2]) || (D1[2] == D2[2] && D1[1] < D2[1]) || (D1[2] == D2[2] && D1[1] == D2[1] && D1[0] < D2[0])) {
+			std::cout << "equal or greater" << std::endl;
+			fact = 1;
+		}
+		else {
+			std::cout << "lesser" << std::endl;
+			fact = 0;
+		}
+		if (fact == 0) {
+			std::swap(D1[0], D2[0]);
+			std::swap(D1[1], D2[1]);
+			std::swap(D1[2], D2[2]);
+		}
+		// counting:
+		if ((D1[1] <= 2) && ((D1[2] % 4 == 0 && D1[2] % 100 != 0) || (D1[2] % 400 == 0))) {
+			differencefirst += 1;
+		}
+		for (int month = D1[1]; month <= 12; month++) {
+			differencefirst += numofdays[month - 1];
+		}
+		differencefirst -= D1[0] - 1;
+
+		for (int year = D1[2] + 1; year < D2[2]; year++) {
+			if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+				differenceall += 366;
+			}
+			else {
+				differenceall += 365;
+			}
+		}
+			
+		if ((D2[1] <= 2) && ((D2[2] % 4 == 0 && D2[2] % 100 != 0) || (D2[2] % 400 == 0))) {
+			differencelast += 1;
+		}
+		for (int month = D2[1]; month <= 12; month++) {
+			differencelast += numofdays[month - 1];
+		}
+		differencelast -=D2[0] - 1;
+		differencelast = 365 - differencelast;
+		if (D2[2] == D1[2]) {
+			std::cout << "Difference is: " << 365 - (365 - differencefirst + 365 - differencelast) << std::endl;
+		}
+		else {
+			std::cout << "Difference is: " << differenceall + differencefirst + differencelast << std::endl;
+		}
+	}
+}
+//7 03 2009
+//22 01 2000
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
