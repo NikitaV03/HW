103c58cbfe7213c159e4e9183430427169721753
Nikita Varygin <85736882+NikitaV03@users.noreply.github.com>
2021-10-25 16:31:50 +0300

    Update classwork.cpp

--- 25.10.21/classwork.cpp
+++ 25.10.21/classwork.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
-void minelements(int n);
+
+int randomarray(int* array, int n, int a=1, int b=100);
+int minelements(int* array, int n);
 double sredarifm(int massiv[], int n);
-double swapbyaverage(int massiv[], int n, double average);
+void swapbyaverage(int massiv[], int n, double average);
+int countallnumbers100(int* array, int n);
 
 int main()
 {
 	int exit;
 	// main code
+
 	int n;
 	std::cout << "Input number of elements in random array: ";
 	std::cin >> n;
-	minelements(n);
+	int* array= new int[n];
+	randomarray(array, n);
+	minelements(array,n);
+	int average = sredarifm(array, n);
+	std::cout << "average = " << average << std::endl;
+	swapbyaverage(array, n, average);
+	for (int i = 0; i < n; ++i) {
+		std::cout << array[i] << std::endl;
+	}
+	std::cout << "most common number is " <<countallnumbers100(array,n) <<std::endl;
 	// exit
 	std::cout << "Type something to leave ";
 	std::cin >> exit;
     return 0;
 }
 
-double sredarifm(int massiv[],int n) {
+double sredarifm(int massiv[],int n) { 
 	int countersrednevo=0;
 	for (int i = 0; i < n; ++i) {
 		countersrednevo += massiv[i];
@@ -26,36 +39,34 @@ double sredarifm(int massiv[],int n) {
 	return average;
 }
 
-double swapbyaverage(int massiv[],int n, double average) {
-	int tmp=0,lesscounter=0;
+void swapbyaverage(int massiv[], int n, double average) {
+	int tmp = 0, lesscounter = 0;
 	for (int i = 0; i < n; ++i) {
 		if (massiv[i] < average)
 			lesscounter += 1;
 	}
 	std::cout << "lesscounter = " << lesscounter << std::endl;
+	int j = 0;
 	for (int i = 0; i < lesscounter; ++i) {
-		int j = 0;
 		while (massiv[i] > average) {
 			tmp = massiv[i];
-			massiv[i] = massiv[n - 1 -j];
+			massiv[i] = massiv[n - 1 - j];
 			massiv[n - 1 - j] = tmp;
 			j += 1;
 		}
 
 	}
-	std::cout << std::endl;
 	std::cout << "After swap" << std::endl;
 	for (int i = 0; i < n; ++i) {
 		std::cout << massiv[i] << std::endl;
 	}
-	return 0;
+	std::cout << std::endl;
+	std::cout << "number of comparisons = " << j << std::endl << std::endl;
 }
 
-void minelements(int n) {
-	int* array = new int[n];
-	int minel = 100, counter = 1;
+int minelements(int* array,int n) {
+	int minel = 2147483647, counter = 1;
 	for (int i = 0; i < n; ++i) {
-		array[i] = rand() % 100 + 1;
 		if (array[i] < minel) {
 			minel = array[i];
 			counter = 1;
@@ -67,9 +78,37 @@ void minelements(int n) {
 	for (int i = 0; i < n; ++i) {
 		std::cout << array[i] << std::endl;
 	}
-	int average = sredarifm(array, n);
-	std::cout << "average = " << average << std::endl;
 	std::cout << "minel = " << minel << std::endl;
 	std::cout << "number of minel = " << counter << std::endl;
-	swapbyaverage(array, n, average);
+	return* array;
 }
+
+int randomarray(int* array, int n, int a, int b) {
+	for (int i = 0; i < n; ++i) {
+		array[i] = rand() % 100 + a;
+	}
+	return *array;
+}
+
+// works only with [1,100]
+int countallnumbers100(int* array, int n) { 
+	int number=0;
+	int numberofnumbers[100];
+	for (int i = 0; i < 100; ++i) {
+		numberofnumbers[i] = 0;
+	}
+	for (int i = 0; i < n; ++i) {
+		numberofnumbers[array[i]-1] += 1;
+	}
+
+	int maxel = 0, ind = -1;
+	for (int i = 0; i < 100; ++i) {
+		if (numberofnumbers[i] > maxel) {
+			maxel = numberofnumbers[i];
+			ind = i;
+		}
+	}
+	number = ind + 1;
+	return number;
+}
+	
