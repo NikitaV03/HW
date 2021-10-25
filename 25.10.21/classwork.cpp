be0f0c2bc3be0f98068bca42aa53517e33caebed
Nikita Varygin <85736882+NikitaV03@users.noreply.github.com>
2021-10-25 12:52:22 +0300

    Update classwork.cpp

--- 25.10.21/classwork.cpp
+++ 25.10.21/classwork.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 void minelements(int n);
 double sredarifm(int massiv[], int n);
+double swapbyaverage(int massiv[], int n, double average);
 
 int main()
 {
@@ -16,9 +17,43 @@ int main()
     return 0;
 }
 
+double sredarifm(int massiv[],int n) {
+	int countersrednevo=0;
+	for (int i = 0; i < n; ++i) {
+		countersrednevo += massiv[i];
+	}
+	double average = double(countersrednevo)/double(n);
+	return average;
+}
+
+double swapbyaverage(int massiv[],int n, double average) {
+	int tmp=0,lesscounter=0;
+	for (int i = 0; i < n; ++i) {
+		if (massiv[i] < average)
+			lesscounter += 1;
+	}
+	std::cout << "lesscounter = " << lesscounter << std::endl;
+	for (int i = 0; i < lesscounter; ++i) {
+		int j = 0;
+		while (massiv[i] > average) {
+			tmp = massiv[i];
+			massiv[i] = massiv[n - 1 -j];
+			massiv[n - 1 - j] = tmp;
+			j += 1;
+		}
+
+	}
+	std::cout << std::endl;
+	std::cout << "After swap" << std::endl;
+	for (int i = 0; i < n; ++i) {
+		std::cout << massiv[i] << std::endl;
+	}
+	return 0;
+}
+
 void minelements(int n) {
 	int* array = new int[n];
-	int minel = 100,counter = 1;
+	int minel = 100, counter = 1;
 	for (int i = 0; i < n; ++i) {
 		array[i] = rand() % 100 + 1;
 		if (array[i] < minel) {
@@ -32,16 +67,9 @@ void minelements(int n) {
 	for (int i = 0; i < n; ++i) {
 		std::cout << array[i] << std::endl;
 	}
-	std::cout << "average = "<< sredarifm(array, n)<< std::endl;
-	std::cout << "minel = " << minel <<std::endl;
+	int average = sredarifm(array, n);
+	std::cout << "average = " << average << std::endl;
+	std::cout << "minel = " << minel << std::endl;
 	std::cout << "number of minel = " << counter << std::endl;
-}
-
-double sredarifm(int massiv[],int n) {
-	int countersrednevo=0;
-	for (int i = 0; i < n; ++i) {
-		countersrednevo += massiv[i];
-	}
-	double average = double(countersrednevo)/double(n);
-	return average;
+	swapbyaverage(array, n, average);
 }
