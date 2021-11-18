b565612cfaa35419dc923be6de48235fef580c79
Nikita Varygin <85736882+NikitaV03@users.noreply.github.com>
2021-11-18 22:46:40 +0300

    Update water.cpp

--- 22.11.21/water.cpp
+++ 22.11.21/water.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include<string>
+#include <string>
+#include <fstream>
 
 template<typename T>
 T** createarraypls(size_t rows, size_t cols) {
@@ -118,6 +119,28 @@ void water(int** a, size_t n, size_t m, size_t steps = 1, size_t ny = 0, size_t 
     }
 }
 
+int arraystxt(int** a, size_t n, size_t m, size_t steps, size_t ny = 0, size_t mx = 0) {
+    std::ofstream outFile("arrays.txt");
+    if (!outFile) {
+        return -1;
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            outFile << a[i][j] << " ";
+        }
+        outFile << std::endl;
+    }
+    water(a, n, m, steps, ny, mx);
+    outFile << std::endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            outFile << a[i][j] << " ";
+        }
+        outFile << std::endl;
+    }
+    outFile.close();
+}
+
 int main()
 {
     int n=-1, m=-1, ny=-1, mx=-1, steps=-1;
@@ -134,7 +157,7 @@ int main()
     randomintarray(terrain, n, m, 10);
     std::cout << "Before flood: " << std::endl;
     printnicearray(terrain, n, m);
-    water(terrain, n, m,steps,ny,mx);
+    water(terrain, n, m, steps, ny, mx); //to get file instead write: arraystxt(terrain, n, m, steps, ny, mx);
     std::cout << "After flood: " << std::endl;
     printnicearray(terrain, n, m);
     deletearray<int>(terrain, n);
