91d3885ee8a8edff13372efad2d32ccababa85a5
Nikita Varygin <85736882+NikitaV03@users.noreply.github.com>
2021-11-21 20:38:57 +0300

    Update classwork.cpp

--- 15.11.21/classwork.cpp
+++ 15.11.21/classwork.cpp
@@ -1,16 +1,6 @@
 
 #include <iostream>
 #include "twodimarrays.h"
-
-template<typename T>
-void test(size_t rows,size_t cols, T yoursymb = 1) {
-    T** arr = createarraypls<T>(rows, cols);
-    constarray<T>(arr, rows, cols, yoursymb);
-    printarray<T>(arr, rows, cols);
-    onesarray<T>(arr, rows, cols);
-    printnicearray<T>(arr, rows, cols, 3);
-    deletearray<T>(arr, rows);
-}
 int main()
 {
     size_t rows, cols;
