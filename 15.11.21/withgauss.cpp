17d191f904e65e42ca5bcd1e453564cc318752a9
Nikita Varygin <85736882+NikitaV03@users.noreply.github.com>
2021-12-18 14:56:11 +0300

    Update withgauss.cpp

--- 15.11.21/withgauss.cpp
+++ 15.11.21/withgauss.cpp
@@ -3,11 +3,10 @@
 #include "twodimarrays.h"
 #include <cmath>
 
-// НЕ РАБОТАЕТ :((((((((((((
 int gaussmatrix(double** matrix, int n) {
     int countswaps = 1;
     for (int i = 0; i < n; ++i) {
-        int maxint = 0;
+        int maxint = i;
         for (int j = i; j < n; ++j) {
             if (abs(matrix[j][i]) > abs(matrix[maxint][i])) {
                 maxint = j;
@@ -16,7 +15,9 @@ int gaussmatrix(double** matrix, int n) {
         if (abs(matrix[maxint][i]) <= 0.00000001) {
             continue;
         }
-        std::swap(matrix[maxint][i], matrix[i][i]);
+        for (int k = i; k < n; ++k) {
+            std::swap(matrix[maxint][k], matrix[i][k]);
+        }
         countswaps++;
         for (int j = i + 1; j < n; ++j)
         {
@@ -29,7 +30,13 @@ int gaussmatrix(double** matrix, int n) {
     return (countswaps%2==1) ? 1 :-1;
 }
 
-
+double gaussdeterminant(double** matrix, int n) {
+    double product = 1;
+    for (int i = 0; i < n; ++i) {
+        product *= matrix[i][i];
+    }
+    return product;
+}
 
 int main()
 {
@@ -58,10 +65,29 @@ int main()
             arr2[i][j] = 0;
         }
     }
+
+    double** arr3 = createarraypls<double>(4, 4);
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            arr3[i][j] = rand() % 10;
+        }
+    }
+    for (int i = 1; i < 2; ++i) {
+        for (int j = 1; j < 2; ++j) {
+            arr3[i][j] = 0;
+        }
+    }
+
     printnicearray<double>(arr, rows, cols, 10);
     gaussmatrix(arr, rows);
+    std::cout << "determinant is:    " << gaussdeterminant(arr, rows) << std::endl;
     printnicearray<double>(arr, rows, cols, 20);
     printnicearray<double>(arr2, 5, 5, 10);
     gaussmatrix(arr2, 5);
+    std::cout << "determinant is:    " << gaussdeterminant(arr2, 5) << std::endl;
     printnicearray<double>(arr2, 5, 5, 20);
+    printnicearray<double>(arr3, 4, 4, 10);
+    gaussmatrix(arr3, 4);
+    std::cout << "determinant is:    " << gaussdeterminant(arr3, 4) << std::endl;
+    printnicearray<double>(arr3, 4, 4, 20);
 }
